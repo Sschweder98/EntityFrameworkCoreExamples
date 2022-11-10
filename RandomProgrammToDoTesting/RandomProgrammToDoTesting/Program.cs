bddebda68cd9c1efe50527ee46cb37ab0671429e
s.schweder <s.schweder1998@gmail.com>
2022-11-10 19:54:37 +0100

    Created some random Programm that we will use for our testing

---
+++ RandomProgrammToDoTesting/RandomProgrammToDoTesting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace RandomProgrammToDoTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static int CountLetterAInSentence(String Sentence)
+        {
+            int Counter = 0;
+            foreach (char Letter in Sentence)
+            {
+                if (Letter == 'A' || Letter == 'a')
+                    Counter++;
+            }
+            return Counter;
+        }
+
+        static Boolean IsLetterBInSentence(String Sentence)
+        {
+            Boolean FoundLetterB = false;
+            foreach (char Letter in Sentence)
+            {
+                if (Letter == 'B' || Letter == 'b')
+                {
+                    FoundLetterB = true;
+                    break;
+                }
+            }
+            return FoundLetterB;
+        }
+    }
+}
