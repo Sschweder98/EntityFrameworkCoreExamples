38c8fd4148f70203d489004f8bace78cd97f5b4e
s.schweder <s.schweder1998@gmail.com>
2022-11-10 13:00:05 +0100

    Created a Class for Hotel Bookings and brought it with an Migration to the Database.
    Created Methods that do the basic stuff of Entity Framework like adding, changing, removing and reading Items from the Database

---
+++ EF_Core_Basics/EF_Core_Basics/Migrations/20221110112345_HotelBooking.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_Core_Basics.Migrations
+{
+    public partial class HotelBooking : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Customer_Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    EmailAdress = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    BookingDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    IsPaymentRecieved = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    RoomNumber = table.Column<int>(type: "int", nullable: false),
+                    BookingNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookings");
+        }
+    }
+}
--- EF_Core_Basics/EF_Core_Basics/Program.cs
+++ EF_Core_Basics/EF_Core_Basics/Program.cs
@@ -1,12 +1,99 @@
 ﻿using System;
+using System.Security.Cryptography;
+using EF_Core_Basics.Migrations;
+using Microsoft.EntityFrameworkCore;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace EF_Core_Basics
 {
     internal class Program
     {
+        static HotelContext _HotelContext = new HotelContext();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //CreateEntries();
+            ChangeEntries();
+            DeleteEntries();
+            GetEntries();
         }
+
+        static void CreateEntries()
+        {
+            _HotelContext.Bookings.Add(new HotelContext.HotelBooking
+            {
+                Customer_Name = "Person A", EmailAdress="PersonA@AnbieterA.com", BookingDate=DateTime.Now, IsPaymentRecieved=false, RoomNumber = 1, BookingNumber=1
+            }) ;
+
+            _HotelContext.Bookings.Add(new HotelContext.HotelBooking
+            {
+                Customer_Name = "Person B", EmailAdress="PersonB@AnbieterA.com", BookingDate=DateTime.Now, IsPaymentRecieved=false, RoomNumber = 2, BookingNumber=2
+            }) ;
+
+            _HotelContext.Bookings.Add(new HotelContext.HotelBooking
+            {
+                Customer_Name = "Person C", EmailAdress="PersonC@AnbieterA.com", BookingDate=DateTime.Now, IsPaymentRecieved=false, RoomNumber = 3, BookingNumber=3
+            }) ;
+            _HotelContext.SaveChanges();
+        }
+
+        static void ChangeEntries() 
+        {
+            var Entry1 = _HotelContext.Bookings.Where(b => b.Customer_Name == "Person B").FirstOrDefault();
+            if (Entry1 != null) {
+                Entry1.IsPaymentRecieved = true;
+                _HotelContext.SaveChanges();
+            }
+
+            var Entry2 = _HotelContext.Bookings.Where(b => b.BookingNumber ==2).FirstOrDefault();
+            if (Entry2 != null)
+            {
+                Entry2.EmailAdress = "PersonB@AnbieterB.com";
+                _HotelContext.SaveChanges();
+            }
+        }
+
+        static void DeleteEntries()
+        {
+            var Entry1 = _HotelContext.Bookings.Where(b => b.Customer_Name == "Person C").FirstOrDefault();
+            if (Entry1 != null)
+            {
+                _HotelContext.Bookings.Remove(Entry1);
+            }
+            _HotelContext.SaveChanges();
+
+        }
+
+        static void GetEntries()
+        {
+            foreach (var booking in _HotelContext.Bookings.Where(b => b.IsPaymentRecieved == true)) {
+                Console.WriteLine(String.Format("Der Kunde {0} hat am {1} das Zimmer {2} gebucht", booking.Customer_Name, booking.BookingDate.Date, booking.RoomNumber));
+            }
+        }
+
+
+
+    }
+}
+
+public class HotelContext : DbContext
+{
+    public DbSet<HotelBooking> Bookings { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        String ConnectionString = "Server=localhost;Database=ef_core_examples;Uid=root;Pwd=test#1234;";
+        var ServerVersion = new MySqlServerVersion(new Version(8, 0, 27));
+        optionsBuilder.UseMySql(ConnectionString, ServerVersion, o => o.MinBatchSize(1).MaxBatchSize(250));
+    }
+
+    public class HotelBooking { 
+    
+        public int Id { get; set; }
+        public string? Customer_Name { get; set; }
+        public string? EmailAdress { get; set; }
+        public DateTime BookingDate { get; set; }
+        public Boolean IsPaymentRecieved { get; set; }
+        public int RoomNumber { get; set; }
+        public int BookingNumber { get; set; }
+
     }
 }
